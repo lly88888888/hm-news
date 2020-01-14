88540c2eddbc470786c2bd5c11e1d3723b4ef9b8
lly88888888 <1261939773@qq.com>
2020-01-14 17:45:19 +0800

    注册功能初步完成(有待优化)

---
+++ src/pages/Register.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="register">
+    <div class="close"
+         @click="close">
+      <i class="iconfont iconicon-test"></i>
+    </div>
+    <div class="logo">
+      <span class="iconfont iconnew"></span>
+    </div>
+    <div class="username">
+      <Hminput v-model="username"
+               type="text"
+               placeholder="请输入用户名/手机号"
+               :rules="/^1\d{4,12}$/"
+               err-msg="账号格式错误"></Hminput>
+    </div>
+    <div class="nickname">
+      <Hminput v-model="nickname"
+               type="text"
+               placeholder="请输入您的昵称"
+               :rules="/^[A-Za-z0-9_\-\u4e00-\u9fa5]+$/"
+               err-msg="请输入合法昵称"></Hminput>
+    </div>
+    <div class="password">
+      <Hminput v-model="password"
+               type="password"
+               placeholder="请输入密码"
+               :rules="/^1\d{2,8}$/"
+               err-msg="密码格式错误"></Hminput>
+    </div>
+    <div class="login">
+      <Hmbutton @click="register">注册</Hmbutton>
+    </div>
+  </div>
+</template>
+
+<script>
+import '../css/font.css'
+import Hminput from '../components/Hminput'
+import Hmbutton from '../components/Hmbutton'
+import axios from 'axios'
+import Vue from 'vue'
+import { Toast } from 'vant'
+Vue.use(Toast)
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      nickname: ''
+    }
+  },
+  components: {
+    Hminput,
+    Hmbutton
+  },
+  methods: {
+    async register () {
+      if (!this.username && !this.password && !this.nickname) return
+      const res = await axios.post('http://localhost:3000/register', {
+        username: this.username,
+        nickname: this.nickname,
+        password: this.password
+      })
+      const { data } = res
+      if (data.statusCode === 400) {
+        Toast(data.message)
+      } else {
+        this.$router.push('/login')
+      }
+    },
+    close () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.register {
+  padding: 20px;
+  i {
+    font-size: 27px;
+  }
+  .logo {
+    text-align: center;
+    color: #d81e06;
+    span {
+      font-size: 127px;
+    }
+  }
+  .password {
+    padding-bottom: 66px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../pages/Login'
+import Register from '../pages/Register'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/register', component: Register }
   ]
 })
 export default router
